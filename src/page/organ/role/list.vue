cd7ab8255b7c6fc164cfd03d5f1218dc4e00e19c
wangdongdong <wangdongdong@passiontec.cn>
2019-02-16 16:16:11 +0800

    冲突

--- src/page/organ/role/list.vue
+++ src/page/organ/role/list.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <el-table
     :data="tableData5"
     style="width: 100%">
@@ -60,23 +59,10 @@
   }
 </style>
 
-=======
-    <el-tree
-      class="filter-tree"
-      :data="data2"
-      :props="defaultProps"
-      default-expand-all
-      :filter-node-method="filterNode"
-      ref="tree2">
-    </el-tree>
-</template>
-
->>>>>>> 17b534d51365b2438c87c322ce9c741fd10e0a84
 <script>
   export default {
     data() {
       return {
-<<<<<<< HEAD
         tableData5: []
       }
     },
@@ -86,82 +72,4 @@
       })
     }
   }
-=======
-        filterText: '',
-        data2: [
-        {
-            "id": "1",
-            "label": "顶级分类",
-            "pId": "0",
-            "children": [
-                {
-                    "id": "2",
-                    "label": "毛线"
-                },
-                {
-                    "id": "4",
-                    "label": "粗线"
-                },
-                {
-                    "id": "2",
-                    "label": "次级分类",
-                    "pId": "1",
-                    "children": [
-                        {
-                            "id": "1",
-                            "label": "电线"
-                        }
-                    ]
-                },
-                {
-                    "id": "3",
-                    "label": "次级分类",
-                    "pId": "1",
-                    "children": [
-                        {
-                            "id": "3",
-                            "label": "细线"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "id": "4",
-            "label": "顶级分类",
-            "pId": "0"
-        }
-    ]
-,
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        //         function(data,node){
-        //       console.log(data,node)
-        //       if(data.pId ||data.pId === 0){
-        //           return data.label
-        //       }else{
-        //           node.visible = false;
-        //       }
-        //   }
-        }
-      };
-    },
-    mounted () {
-        this.$refs.tree2.filter('val');
-    },
-    methods: {
-        filterNode (value, data ,ff) {
-            // return false
-            console.log(value, data, ff,88888)
-            if(data.pId || data.pId ==='0'){
-                return true
-            }
-        },
-        aa (data,node){
-            console.log(data,node)
-        }
-    }
-  };
->>>>>>> 17b534d51365b2438c87c322ce9c741fd10e0a84
 </script>
