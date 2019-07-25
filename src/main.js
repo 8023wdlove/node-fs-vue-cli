93e578afb606cd6f7c65f54e71a6a798fced0d66
wangdongdong <wangdongdong@dianxiaohuo.net>
2019-07-25 17:03:12 +0800

    2222

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 // import router from './router'
 import routes from '@/router/index.js'
+var Parse = require('parse')
+Parse.initialize('b32h7SrhRXGiD3Ubvt2KQHtiR3VPrPgYWIxO3l5Z')
+Parse.serverURL = 'https://gesoo.herokuapp.com/parse'
+console.log(Parse._getInstallationId().then((res)=>{
+  console.log(res,888)
+}),Parse,77888)
 import ElementUI from 'element-ui'
 
 import '@assets/styles/default.css'
@@ -11,8 +18,20 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
 let router = routes.routes
+Vue.prototype.$axios = axios;
+Vue.prototype.$Parse = Parse;
 
 Vue.config.productionTip = false
+// 注册一个全局自定义指令 `v-focus`
+Vue.directive('focus', {
+  // 当被绑定的元素插入到 DOM 中时……
+  inserted: function (el, binding, vnode) {
+    // 聚焦元素
+    console.log(el, 888);
+    el.focus();
+    el.style.backgroundColor='green'; 
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/page/organ/itemInfo/list.vue
+++ src/page/organ/itemInfo/list.vue
@@ -1,20 +1,72 @@
-
 <template>
   <div>
     <span>我是商品资料列表</span>
     <div>
-      <button @click = "jump ('/organ/itemInfo/info/666')" >列表详情</button>
+       <el-input v-model="input" placeholder="请输入内容"></el-input>
+       <el-checkbox-group v-model="checkList">
+        <el-checkbox v-for="item,index in data" :label="item.id" :key="item.id">{{item.name + '' +item.tel}}</el-checkbox>
+      </el-checkbox-group>
     </div>
   </div>
 </template>
 <script>
 export default {
-  methods: {
-    jump (data) {
-      this.$router.push({
-        path: data
+  /* eslint-disable */
+  data () {
+    return {
+      data: [{id: 1, name: '王一', tel: 123131}, {id: 2, name: '网二', tel: 123131}, {id: 3, name: '网伞', tel: 123131}, {id: 4, name: '哈哈', tel: 123131}, {id: 5, name: '呵呵', tel: 123131}],
+      checkList: [],
+      input: ''
+    }
+  },
+  watch: {
+    input (val, old) {
+      this.checkList = []
+      if (val) {
+        let arr = []
+        arr = this.data.filter((item, index) => {
+          return item.name.indexOf(val) !== -1 || (item.tel + '').indexOf(val) !== -1
+        })
+        arr.forEach((item, index, datas) => {
+          this.checkList.push(item.id)
+        })
+      }
+    }
+  },
+  created (){
+    this.login();
+  },
+  methods:{
+    login () {
+      let that = this;
+      this.$axios.post('https://gesoo2.herokuapp.com/parse/functions/userLogin',{
+         username: 'fxchou123',
+        password: '123456',
+        language: 'cn'
+      },
+      {headers: {
+      "Content-Type": "application/json",
+      "X-Parse-Application-Id":"b32h7SrhRXGiD3Ubvt2KQHtiR3VPrPgYWIxO3l5Z"
+      }}).then((res)=>{
+        console.log(res.data.result.token)
+        that.$Parse.User.become(res.data.result.token).then((success) => {
+        console.log(success, 8888)
+        console.log(that.$Parse.User)
+      }, (err) => {
+        console.log(err, 899000)
+      })
       })
     }
+    
   }
 }
 </script>
+<style lang="less">
+  .el-checkbox__input.is-checked+.el-checkbox__label{
+    color:black;
+  }
+  .el-checkbox__input.is-checked .el-checkbox__inner, .el-checkbox__input.is-indeterminate .el-checkbox__inner{
+    background: greenyellow;
+    border-color:greenyellow;
+  }
+</style>
